21fad273d74f1af3bcc7b6666cccb1c3cdb823be
Eric Douglass <dougli1sqrd@gmail.com>
2024-04-17 10:58:14 -0700

    doing some hard coding

--- jekyll-build.sh
+++ jekyll-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -euo pipefail
 
-docker build . -f docker/Dockerfile.BuildJekyll --build-arg gid=${SUDO_GID} --build-arg gname=$(getent group $SUDO_GID | cut -d: -f1) --build-arg uid=${SUDO_UID} --build-arg uname=${SUDO_USER} "$@"
+docker build . -f docker/Dockerfile.BuildJekyll --build-arg gid=1000 --build-arg uid=1000 "$@"
 
